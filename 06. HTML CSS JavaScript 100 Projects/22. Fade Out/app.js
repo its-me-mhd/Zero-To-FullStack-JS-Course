fe3a9da78581f97fc47a44eb35c1c099ecfce1c0
it's me <mohamedabdirisaaq57@gmail.com>
2024-10-27 20:14:39 +0600

    Updated app.js

--- 06. HTML CSS JavaScript 100 Projects/22. Fade Out/app.js
+++ 06. HTML CSS JavaScript 100 Projects/22. Fade Out/app.js
@@ -1,10 +1,12 @@
 let arrow = document.getElementById("arrow");
 
 window.addEventListener("scroll", () => {
-  let position = window.scrollY;
-  // console.log(position);
-  if (position <= 5) {
-    arrow.classList.toggle("fade-in");
-    arrow.classList.toggle("fade-out");
+  if (window.scrollY <= 5) {
+    arrow.classList.toggle("fade-in", true);
+    arrow.classList.toggle("fade-out", false);
+  } else {
+    arrow.classList.toggle("fade-in", false);
+    arrow.classList.toggle("fade-out", true);
   }
 });
+
